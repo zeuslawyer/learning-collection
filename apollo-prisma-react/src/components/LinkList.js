f89bad05a92686ca0d1e0f0586da6968c97068f2
zeuslawyer <zubin.pratap@gmail.com>
2019-08-17 21:17:00 +1000

    refactor linklist render prop

--- apollo-prisma-react/src/components/LinkList.js
+++ apollo-prisma-react/src/components/LinkList.js
@@ -19,36 +19,23 @@ const FEED_QUERY = gql`
 `;
 
 function LinkList() {
-  const linksToRender = [
-    {
-      id: '1',
-      description: 'Prisma turns your database into a GraphQL API 😎',
-      url: 'https://www.prismagraphql.com'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
-    }
-  ];
+  const renderList = ({ loading, error, data }) => {
+    if (loading) return <div>Fetching</div>;
+    if (error) return <div>Error</div>;
 
+    const linksToRender = data.feed.links;
 
+    return (
+      <div>
+        {linksToRender.map(link => (
+          <Link key={link.id} link={link} />
+        ))}
+      </div>
+    );
+  }
   return (
-    <Query query={FEED_QUERY}>
-      {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>
-          if (error) return <div>Error</div>
-    
-          const linksToRender = data.feed.links
-    
-          return (
-            <div>
-              {linksToRender.map(link => <Link key={link.id} link={link} />)}
-            </div>
-          )
-        }}
-    </Query>
-  )
+    <Query query={FEED_QUERY} render={renderList} />
+  );
 }
 
 export default LinkList;
